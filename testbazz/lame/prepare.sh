dc39fb6242b9c749d18ca3727c51001f5bedf7f5
BazzAFL <bazzafl@163.com>
2022-09-02 17:32:14 +0800

    v1.0.0

--- testbazz/lame/prepare.sh
+++ testbazz/lame/prepare.sh
@@ -1,4 +1,4 @@
-TARGET=tiffsplit
+TARGET=lame
 FUZZER=afl-fuzz
 cd ../.. && make $FUZZER
 cp -f $FUZZER testbazz/$TARGET/$FUZZER
@@ -6,4 +6,4 @@ cd testbazz/$TARGET
 
 # rm -rf out/
 
-# AFL_NO_UI=1 timeout 24h ./afl-fuzz -i in_tif -z 4 -o out ./tiffsplit @@
+# AFL_NO_UI=1 timeout 24h ./afl-fuzz -i in_wav -z 4 -o out ./lame @@ /dev/null
